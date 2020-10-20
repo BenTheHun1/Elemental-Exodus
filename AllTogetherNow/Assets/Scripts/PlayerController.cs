3afc12ed05f5964e7429debcd46fc2d46f6042b6
Benjamin Hunsicker <benhunsicker1@gmail.com>
2020-10-19 20:16:12 -0400

    Updated Movement

--- AllTogetherNow/Assets/Scripts/PlayerController.cs
+++ AllTogetherNow/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float xInput;
-    public float yInput;
-    public float speed = 20;
+    public Vector2 movement;
+    public float speed;
     public GameObject camera;
-    private Rigidbody2D rb2d;
+    public Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = gameObject.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxis("Horizontal");
-        yInput = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        rb2d.MovePosition(Vector2.up * Time.deltaTime * speed * yInput);
+    }
 
-       // transform.Translate(Vector2.up * Time.deltaTime * speed * yInput);
-        transform.Translate(Vector2.right * Time.deltaTime * speed * xInput);
-
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + Time.fixedDeltaTime * speed * movement);
+        rb2d.MovePosition(rb2d.position + Time.fixedDeltaTime * speed * movement);
     }
 
     private void LateUpdate()
