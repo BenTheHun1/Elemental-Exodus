8b78f63df37fa6123585367a5f7cc3e04052fab9
David <Daver1012@gmail.com>
2020-10-21 01:52:02 -0400

    Fixed jumping when swimming, walking on the floor and platforms slow you down, added a door that goes to the next scene, and added camera manipulation to follow the player for both horizontal and vertical perspectives.

--- AllTogetherNow/Assets/Scripts/PlayerControllerPlatformer.cs
+++ AllTogetherNow/Assets/Scripts/PlayerControllerPlatformer.cs
@@ -4,31 +4,98 @@ using UnityEngine;
 
 public class PlayerControllerPlatformer : MonoBehaviour
 {
-	
-	public float Speed = 20f;
-	
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject camera;
+	public float Speed;
+	public float JumpPower;
+	public bool horizontalaxis;
+	public bool verticalaxis;
+	public Vector3 MinCameraPos;
+	public Vector3 MaxCameraPos;
+	public Rigidbody2D rigidbody2d;
 
-    // Update is called once per frame
     void Update()
     {
 		Jump();
 		float DirectionX = Input.GetAxis ("Horizontal") * Speed * Time.deltaTime;
 		transform.position = new Vector2 (transform.position.x + DirectionX, transform.position.y);
-	   
+		
+		if (horizontalaxis)
+		{
+          camera.transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinCameraPos.x, MaxCameraPos.x), 1, -1);
+		}
+		
+		if (verticalaxis)
+		{
+          camera.transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinCameraPos.x, MaxCameraPos.x), Mathf.Clamp(transform.position.y, MinCameraPos.y, MaxCameraPos.y), -1);
+		}
+		
     }
 	
 	void Jump()
     {
-		
 		if (Input.GetButtonDown("Jump"))
 		{
-			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,Speed),ForceMode2D.Impulse);
+			Speed = 8f;
+			rigidbody2d.velocity = Vector2.up * JumpPower * 1.3f;
 		}
 	   
     }
+	
+	void OnCollisionExit2D (Collision2D collision)
+	{
+	
+		if (collision.gameObject.name == "Sand")
+		{
+				Speed = 8f;
+		   
+        }
+	
+	}
+	
+		
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.name == "Sand")
+        {
+			foreach(ContactPoint2D hitPos in collision.contacts)
+			{
+				if (hitPos.normal.y > 0)
+				{
+				Speed = 5f;
+				}
+			}
+		   
+		   
+        }
+	}
+	
+	void OnTriggerStay2D(Collider2D collision)
+    {	
+		
+		if (collision.gameObject.name == "ChangeCameraPerspectiveHorizontal")
+        {
+			MinCameraPos = new Vector3(-1,-46,-1);
+			MaxCameraPos = new Vector3(137,-21,-1);
+			horizontalaxis = true;
+			verticalaxis = false;
+        }
+		
+		if (collision.gameObject.name == "ChangeCameraPerspectiveVerticalBottom")
+        {
+			MinCameraPos = new Vector3(-1,-46,-1);
+			MaxCameraPos = new Vector3(137,-21,-1);
+			verticalaxis = true;
+			horizontalaxis = false;
+        }
+		
+		if (collision.gameObject.name == "ChangeCameraPerspectiveVerticalTop")
+        {
+			MinCameraPos = new Vector3(-1,24,-1);
+			MaxCameraPos = new Vector3(137,43,-1);
+			verticalaxis = true;
+			horizontalaxis = false;
+        }
+		
+    }
+	
 }
